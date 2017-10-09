6250e618f999588075057a38450e5a472acef545
cocoa <31022407+cocoasui@users.noreply.github.com>
2017-10-09 14:25:53 +0800

    supplement CS.UserAgent
    add [IsIPhone] to CS.UserAgent, [IsIPhone] incidates whether the device is IPhone or not.

--- js/CS.UserAgent.js
+++ js/CS.UserAgent.js
@@ -6,10 +6,19 @@ CS.UserAgent = {
      * 是否为移动设备。
      * Boolean 类型。
      */
-    IsMobile:false
+    IsMobile:false,
+
+    /**
+     * 是否为 IPhone。
+     * Boolean 类型。
+     */
+    IsIPhone:false,
 };
 
 (function (){
-    var regExp = / mobile[ \/]/i;
+    var regExp = /mobile[ \/]/i;
     CS.UserAgent.IsMobile = regExp.test(navigator.userAgent);
+
+    var regExp = /iphone/i;
+    CS.UserAgent.IsIPhone = regExp.test(navigator.userAgent);
 })();
